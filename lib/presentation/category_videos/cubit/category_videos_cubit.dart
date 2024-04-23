4bc99c33a98afaafb6625a0575b7884fa076ea86
cristh18 <dar_funeral18@hotmail.com>
2024-04-23 00:08:38 -0500

    Show category videos metadata

--- lib/presentation/category_videos/cubit/category_videos_cubit.dart
+++ lib/presentation/category_videos/cubit/category_videos_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:logger/logger.dart';
 
+import '../../../data/datasources/remote/dtos/category_videos_response_dto.dart';
 import '../../../data/datasources/remote/dtos/video_data_response_dto.dart';
 import '../../../data/repositories/category_videos_repository.dart';
 
@@ -16,10 +18,16 @@ class CategoryVideosCubit extends Cubit<CategoryVideosState> {
   Future<void> getCategoryVideos(String url) async {
     emit(state.copyWith(status: CategoryVideosStatus.loading));
     try {
-      await categoryVideosRepository.getCategoryVideos(url);
+      final CategoryVideosResponseDto categoryVideosResponseDto =
+          await categoryVideosRepository.getCategoryVideos(url);
+      final Logger logger = Logger();
+      for (final VideoDataResponseDto element
+          in categoryVideosResponseDto.data) {
+        logger.i(element.toString());
+      }
       emit(state.copyWith(
           status: CategoryVideosStatus.success,
-          videos: <VideoDataResponseDto>[]));
+          videos: categoryVideosResponseDto.data));
     } catch (e) {
       emit(state.copyWith(status: CategoryVideosStatus.error));
     }
