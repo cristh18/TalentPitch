a040a80e72dd4c47d2628329ee1936102c0abd7e
cristh18 <dar_funeral18@hotmail.com>
2024-04-29 00:09:04 -0500

    Validate connected field nulability

--- lib/utils/general_util.dart
+++ lib/utils/general_util.dart
@@ -1,21 +1,11 @@
 import 'package:logger/logger.dart';
 
-String getConnectedValue(Object obj) {
-  try {
-    if (obj is String) {
-      return obj;
-    } else if (obj is int) {
-      return obj.toString();
-    } else if (obj is double) {
-      return obj.toString();
-    } else if (obj is bool) {
-      return obj.toString();
-    } else {
-      return '';
-    }
-  } catch (e) {
+String getConnectedValue(Object? obj) {
+  if (obj is String || obj is int || obj is double || obj is bool) {
+    return obj.toString();
+  } else {
     final Logger logger = Logger();
-    logger.e(e);
+    logger.e('Unsupported data type: ${obj.runtimeType}');
     return '';
   }
 }
