16f7bd8859f4c2aaa23e7a41edaabb4b6be00828
cristh18 <dar_funeral18@hotmail.com>
2024-06-12 23:08:04 -0500

    Code refactoring

--- lib/presentation/pitch_detail/ui/pitch_detail_page.dart
+++ lib/presentation/pitch_detail/ui/pitch_detail_page.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../data/repositories/favorite_talents_repository.dart';
-import '../../../di/service_locator.dart';
 import '../../../domain/models/pitch_model.dart';
-import '../cubit/pitch_detail_cubit.dart';
 import 'pitch_detail_view.dart';
 
 class PitchDetailPage extends StatelessWidget {
@@ -14,9 +10,6 @@ class PitchDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<PitchDetailCubit>(
-        create: (BuildContext context) => PitchDetailCubit(
-            favoriteTalentsRepository: locator<FavoriteTalentsRepository>()),
-        child: PitchDetailView(pitchModel: pitchModel));
+    return PitchDetailView(pitchModel: pitchModel);
   }
 }
