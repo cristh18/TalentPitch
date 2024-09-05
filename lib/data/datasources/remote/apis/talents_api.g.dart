6a577ea0d38903440d9622d3d1f372a33994d252
cristh18 <dar_funeral18@hotmail.com>
2024-09-04 20:47:30 -0500

    Update some dependencies

--- lib/data/datasources/remote/apis/talents_api.g.dart
+++ lib/data/datasources/remote/apis/talents_api.g.dart
@@ -6,17 +6,20 @@ part of 'talents_api.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
 
 class _TalentsApi implements TalentsApi {
   _TalentsApi(
     this._dio, {
     this.baseUrl,
+    this.errorLogger,
   });
 
   final Dio _dio;
 
   String? baseUrl;
+
+  final ParseErrorLogger? errorLogger;
 
   @override
   Future<TalentsDto> getTalents() async {
@@ -24,25 +27,31 @@ class _TalentsApi implements TalentsApi {
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
     const Map<String, dynamic>? _data = null;
-    final _result = await _dio
-        .fetch<Map<String, dynamic>>(_setStreamType<TalentsDto>(Options(
+    final _options = _setStreamType<TalentsDto>(Options(
       method: 'GET',
       headers: _headers,
       extra: _extra,
     )
-            .compose(
-              _dio.options,
-              '/api/homeservice/categories/all/12',
-              queryParameters: queryParameters,
-              data: _data,
-            )
-            .copyWith(
-                baseUrl: _combineBaseUrls(
-              _dio.options.baseUrl,
-              baseUrl,
-            ))));
-    final value = TalentsDto.fromJson(_result.data!);
-    return value;
+        .compose(
+          _dio.options,
+          '/api/homeservice/categories/all/12',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late TalentsDto _value;
+    try {
+      _value = TalentsDto.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
   }
 
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
